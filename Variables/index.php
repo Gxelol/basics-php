1920e72debf29084cbe5836cbcc256eda9ce3455
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2023-01-31 15:53:15 -0300

    variables

---
+++ Variables/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title></title>
+</head>
+<body>
+  
+  <?php
+
+    $personName = "Lara";
+    $personAge = 26;
+    
+    echo "<p>$personName is a person</p>";
+    echo "<p>$personName is $personAge years old</p>";
+    $personName = "Paul";
+    $personAge = 16;
+    echo "<p>$personName want a lot of things</p>";
+    echo "<p>$personAge years ago $personName was born</p>";
+  
+  ?>
+
+</body>
+</html>
