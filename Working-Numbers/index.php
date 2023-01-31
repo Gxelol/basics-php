2032046b6f1a08f1703a2f5853419d5e3339dd70
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2023-01-31 19:44:38 -0300

    working with numbers

---
+++ Working-Numbers/index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title></title>
+</head>
+<body>
+  
+  <?php
+  $num = 30;
+  $num++;
+  echo $num;
+
+  echo "<br>";
+
+  $num--;
+  echo $num;
+  
+  echo "<br>";
+  
+  echo 10;
+
+  echo "<br>";
+  
+  echo -10;
+  
+  echo "<br>";
+  
+  echo 3.203;
+  
+  echo "<br>";
+
+  echo "
+  <p>addition +</p>
+  <p>subtraction -</p>
+  <p>multiplication *</p>
+  <p>division /</p>
+  <p>remainder %</p>
+  ";
+  
+  echo "<br>";
+  
+  echo abs(-50);
+
+  echo "<br>";
+  
+  echo pow(2, 5);
+  
+  echo "<br>";
+  
+  echo sqrt(16);
+  
+  echo "<br>";
+
+  echo max(10, 100);
+
+  echo "<br>";
+
+  echo min(10, 100);
+  
+  echo "<br>";
+  
+  echo round(1.928);
+
+  echo "<br>";
+
+  echo ceil(1.128);
+
+  echo "<br>";
+  
+  echo floor(1.928);
+
+  #  $num = $num + 25 === $num += 25;  
+  #  $num = $num - 25 === $num -= 25;  
+  #  $num = $num * 25 === $num *= 25;  
+  ?>
+
+</body>
+</html>
