28fc88eb557650fc5187b50beac860f258b91bfc
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2023-01-31 15:31:27 -0300

    writing html with php

---
+++ Writing-HTML/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title></title>
+</head>
+<body>
+  
+  <?php
+   # echo("Hello World");
+
+    echo"
+    <h1>This is a h1</h1>
+    <hr>
+    <p>Something something, this is some text.</p>
+    ";
+    
+    echo"
+    <h2 style='color: blue'>I'm blue</h2>
+    <p style='color: red'>This paragraph will be red.</p>
+    ";
+  ?>
+
+</body>
+</html>
