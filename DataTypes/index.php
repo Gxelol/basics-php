b0032a1f1886c2ba8aecb10713089053cf08213b
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2023-01-31 16:01:08 -0300

    dataTypes

---
+++ DataTypes/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title></title>
+</head>
+<body>
+  
+  <?php
+
+    $text = "This is a string, text."; #strings
+    
+    $age = 20; #integers 
+    
+    $grade = 10.45; #floats
+
+    $isValid = true; #boolean
+
+    $noValue = null; #null
+
+    echo "
+    <p>$text</p>
+    <p>$age</p>
+    <p>$grade</p>
+    <p>$isValid</p>
+    <p>$noValue</p>
+    "
+  ?>
+
+</body>
+</html>
