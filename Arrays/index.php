1ce96eefd6bb0a0c282bc0c6d8cf1cf095ab0e70
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2023-02-02 15:06:21 -0300

    learning basics of array

---
+++ Arrays/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title></title>
+</head>
+<body>
+
+  <?php
+    $array = [
+      "foo" => "bar",
+      "Person" => "Gabriel"
+    ];
+
+    // echo($array["foo"]);
+
+    $people = ["Gabriel", "Tom", "Prince"];
+    $people[3] = "Lara";
+    echo count($people); 
+  ?>
+
+</body>
+</html>
