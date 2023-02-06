b1def4b8c75c8ee2265764dfd80c338485962c28
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2023-02-06 19:37:34 -0300

    for loops

---
+++ For-loops/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+  <?php
+    $array = [1, 19, 40, 8, 38, 51, 43, 25];
+
+    for($i = 0; $i < count($array); $i++) {
+      echo "$array[$i] <br>";
+    }
+  ?>
+
+</body>
+</html>
